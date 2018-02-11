6255d7ba525fcfc44140133fdc46114beb8c9b98
Brian <ballerbrian4@gmail.com>
2018-02-11 08:43:28 -0800

    maze script created

---
+++ Project1/maze.py
@@ -0,0 +1,34 @@
+#
+import math
+import sys
+import random
+
+welcome = "Welcome to the maze generation. \n"
+rowsA = input("Please input the amount of rows: ")
+columnsA = input("Please input the amount of columns: ")
+
+print (rowsA)
+print (columnsA)
+
+
+
+#variables 
+rowSize = (int(rowsA) * 2) + 1
+columnSize=(int(columnsA) * 2) +1
+
+
+
+print(rowSize)
+print(columnSize)
+#variables to print how 
+cornerChar = "+"
+horizontalChar = "-"
+verticalChar = "|"
+
+#node class for the Binary space partition tree
+#class node:
+#    coordinate = 
+
+#def binarySP(rowsA, columnsA):
+
+
