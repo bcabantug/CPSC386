0976fb4905cdadcc980bae164a0cf7c50698d96d
Brian <ballerbrian4@gmail.com>
2018-02-12 15:09:07 -0800

    added more depth to node class and bsp function

--- Project1/maze.py
+++ Project1/maze.py
@@ -3,6 +3,7 @@ import math
 import sys
 import random
 
+#introduction
 welcome = "Welcome to the maze generation. \n"
 rowsA = input("Please input the amount of rows: ")
 columnsA = input("Please input the amount of columns: ")
@@ -13,22 +14,69 @@ print (columnsA)
 
 
 #variables 
-rowSize = (int(rowsA) * 2) + 1
-columnSize=(int(columnsA) * 2) +1
+#rowSize = (int(rowsA) * 2) + 1
+#columnSize=(int(columnsA) * 2) +1
 
 
 
-print(rowSize)
-print(columnSize)
-#variables to print how 
+#print(rowSize)
+#print(columnSize)
+
+# global variables to print maze character 
 cornerChar = "+"
 horizontalChar = "-"
 verticalChar = "|"
 
-#node class for the Binary space partition tree
-#class node:
-#    coordinate = 
+#node class for the Binary space partition tree (eventually all leaves will be in order and will be able to draw out the maze)
+class Node:
+    #node constructor
+    def __init__(self, beginHRange, endHRange, beginVRange, endVRange, left = none, right = none):
+        self.coordinates = [[beginHRange,endHRange], [beginVRange,endVRange]]
+        self.leftChild = left #left child 
+        self.rightChild = right #right child
 
-#def binarySP(rowsA, columnsA):
+#recursive function to construct the BSP tree
+def binarySP(rows, columns, currNode):
+    
+    #check if split still needs to happen (check if cell is already (total distance of 1 for height and width))
+    if currNode.coordinates[0[1]]-currNode.coordinates[0[0]] == 1 && currNode.coordinates[1[1]]-currNode.coordinates[1[0]] == 1:
+         #if current section is already part of the minimum size of cell 
+         #then stop the split and return to previous parent node to continue the split
+         return
 
 
+    else:
+        selectRandSplit = randInt(1,2)
+        print selectRandSplit    #outputs the selected split direction
+
+    
+
+        #if split happens horizontally
+        if selectRandSplit == 1:
+            #get the random coordinate between the range of the current node to split betweeen for the children
+            randSplit = randint(currNode.beginHRange, rows)
+            #set the two nodes child nodes
+            currNode.split1 = Node(currNode.coordinates[0[0]],randSplit,currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
+            currNode.split2 = Node(randSplit+1, currNode.coordinates[0[1]],currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
+
+            #start the recursive calls for the child nodes to develop the granchildren
+            binarySP(currNode.split1.endHRange, currNode.split1.endVRange, currNode.split1)
+            binarySP(currNode.split2.endHRange, currNode.split2.endVRange, currNode.split2)
+
+        #if split happens vertically    
+        elif selectRandSplit == 2:
+            #get the random coordinate between the range of the current node to split between for the children
+            randSplit = randint(currNode.beginVRange, columns)
+             #set the two nodes child nodes
+            currNode.split1 = Node(currNode.coordinates[0[0]],randSplit,currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
+            currNode.split2 = Node(randSplit+1, currNode.coordinates[0[1]],currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
+
+            #start the recursive calls for generating the next child nodes
+            binarySP(currNode.split1.endHRange, currNode.split1.endVRange, currNode.split1)
+            binarySP(currNode.split2.endHRange, currNode.split2.endVRange, currNode.split2)
+
+
+
+
+
+#beginSpace = Node(int(rowsA), int(columnsA)
