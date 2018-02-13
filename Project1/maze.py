330c00159a9e849bf584cde48a31a557f46358e1
Brian <ballerbrian4@gmail.com>
2018-02-13 00:49:19 -0800

    Adjusted bsp algorithm for developing tree

--- Project1/maze.py
+++ Project1/maze.py
@@ -8,8 +8,8 @@ welcome = "Welcome to the maze generation. \n"
 rowsA = input("Please input the amount of rows: ")
 columnsA = input("Please input the amount of columns: ")
 
-print (rowsA)
-print (columnsA)
+rowsI = int(rowsA)
+columnsI = int(columnsA)
 
 #variables 
 #rowSize = (int(rowsA) * 2) + 1
@@ -25,7 +25,7 @@ print (columnsA)
 #node class for the Binary space partition tree (eventually all leaves will be in order and will be able to draw out the maze)
 class Node:
     #node constructor
-    def __init__(self, beginHRange, endHRange, beginVRange, endVRange, left = none, right = none):
+    def __init__(self, beginHRange, endHRange, beginVRange, endVRange, left=None, right=None):
         self.coordinates = [[beginHRange,endHRange], [beginVRange,endVRange]]
         self.leftChild = left #left child 
         self.rightChild = right #right child
@@ -33,48 +33,75 @@ class Node:
 #recursive function to construct the BSP tree
 def binarySP(rows, columns, currNode):
     
+    print(currNode.coordinates)
+
     #check if split still needs to happen (check if cell is already (total distance of 1 for height and width))
-    if currNode.coordinates[0[1]]-currNode.coordinates[0[0]] == 1 && currNode.coordinates[1[1]]-currNode.coordinates[1[0]] == 1:
+    if currNode.coordinates[0][1]-currNode.coordinates[0][0] == 1 or currNode.coordinates[1][1]-currNode.coordinates[1][0] == 1:
          #if current section is already part of the minimum size of cell 
          #then stop the split and return to previous parent node to continue the split
-         return
+         return None
 
-    print currNode.coordinates
+    #print(currNode.coordinates)
 
     else:
-        selectRandSplit = randint(1,2)
-        print selectRandSplit    #outputs the selected split direction
-
-    
+        selectRandSplit = random.randint(1,2)
+        print (str(selectRandSplit) + "\n\n")    #outputs the selected split direction
 
         #if split happens horizontally
         if selectRandSplit == 1:
             #get the random coordinate between the range of the current node to split betweeen for the children
-            randSplit = randint(currNode.beginHRange, rows)
+            #using +1 -1 to account for bounderies of the end coordinates to prevent selection of self coordinates and infinitely recurr
+            
+            
+            randSplit = random.randint(currNode.coordinates[0][0]+1, rows-1)
+            
+            #adjust split for situation where a number is picked such that it is equal to the last edge of the rows
+            #if randSplit+1 == currNode.coordinates[0][1] or randSplit == currNode.coordinates[0][0]:
+            #    randSplit = random.randint(currNode.coordinates[0][0]+1, rows-1) #untested condition
+
+            
+
+            print("rand "+str(randSplit) + "\n") #outputs the coordinate that will be split among them
             #set the two nodes child nodes
-            currNode.split1 = Node(currNode.coordinates[0[0]],randSplit,currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
-            currNode.split2 = Node(randSplit+1, currNode.coordinates[0[1]],currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
+            
+            
+            currNode.split1 = Node(currNode.coordinates[0][0],randSplit,currNode.coordinates[1][0] ,currNode.coordinates[1][1])
+            print(currNode.split1.coordinates)
+            currNode.split2 = Node(randSplit+1, currNode.coordinates[0][1],currNode.coordinates[1][0] ,currNode.coordinates[1][1])
+            print(currNode.split2.coordinates)
 
             #start the recursive calls for the child nodes to develop the granchildren
-            binarySP(currNode.split1.endHRange, currNode.split1.endVRange, currNode.split1)
-            binarySP(currNode.split2.endHRange, currNode.split2.endVRange, currNode.split2)
+            print ("split1RandH")
+            binarySP(currNode.split1.coordinates[0][1], currNode.split1.coordinates[1][1], currNode.split1)
+            print ("split2RandH")
+            binarySP(currNode.split2.coordinates[0][1], currNode.split2.coordinates[1][1], currNode.split2)
 
         #if split happens vertically    
         elif selectRandSplit == 2:
             #get the random coordinate between the range of the current node to split between for the children
-            randSplit = randint(currNode.beginVRange, columns)
-             #set the two nodes child nodes
-            currNode.split1 = Node(currNode.coordinates[0[0]],randSplit,currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
-            currNode.split2 = Node(randSplit+1, currNode.coordinates[0[1]],currNode.coordinates[1[0]] ,currNode.coordinates[1[1]])
+            randSplit = random.randint(currNode.coordinates[1][0]+1, columns-1)
+
+            print("rand "+str(randSplit) + "\n")
+
+            
+            #currNode.split1 = Node(currNode.coordinates[0][0],randSplit,currNode.coordinates[1][0] ,currNode.coordinates[1][1])
+            #currNode.split2 = Node(randSplit+1, currNode.coordinates[0][1],currNode.coordinates[1][0] ,currNode.coordinates[1][1])
+            currNode.split1 = Node(currNode.coordinates[0][0], currNode.coordinates[0][1],currNode.coordinates[1][0] ,randSplit)
+            print(currNode.split1.coordinates)
+
+            currNode.split2 = Node(currNode.coordinates[0][0], currNode.coordinates[0][1],randSplit+1,currNode.coordinates[1][1])
+            print(currNode.split2.coordinates)
 
             #start the recursive calls for generating the next child nodes
-            binarySP(currNode.split1.endHRange, currNode.split1.endVRange, currNode.split1)
-            binarySP(currNode.split2.endHRange, currNode.split2.endVRange, currNode.split2)
+            print ("split1RandV")
+            binarySP(currNode.split1.coordinates[0][1], currNode.split1.coordinates[1][1], currNode.split1)
+            print ("split2RandV")
+            binarySP(currNode.split2.coordinates[0][1], currNode.split2.coordinates[1][1], currNode.split2)
 
 
 
-treeRoot = Node(0, int(rowsA), 0, int(columnsA),none, none)
+treeRoot = Node(0, rowsI, 0, columnsI)
 
-binarySP(int(rowsA), int(columnsA), treeRoot)
+binarySP(rowsI, columnsI, treeRoot)
 
 #beginSpace = Node(int(rowsA), int(columnsA)
