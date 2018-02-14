f7edb1d963ca6c8186ff6af929fbdb80f59e5c20
Brian <ballerbrian4@gmail.com>
2018-02-14 10:19:02 -0800

    bsp tree splitting evenly

--- Project1/maze.py
+++ Project1/maze.py
@@ -9,8 +9,8 @@ rowsA = input("Please input the amount of rows: ")
 columnsA = input("Please input the amount of columns: ")
 
 #takes the input and multiplies it by two to get the full range of the maze to calculate
-rowsI = int(rowsA*2)
-columnsI = int(columnsA*2)
+rowsI = int(rowsA)*2
+columnsI = int(columnsA)*2
 
 #variable to hold coordinates for portals when generating maze
 mazePortals = []
@@ -39,6 +39,7 @@ class Node:
 #recursive function to construct the BSP tree
 def binarySP(rows, columns, currNode):
     
+    print("currCoordinates:")
     print(currNode.coordinates)
 
     #check if split still needs to happen (check if cell is already (total distance of 1 for height and width))
@@ -62,16 +63,28 @@ def binarySP(rows, columns, currNode):
        #get the random coordinate between the range of the current node to split betweeen for the children            
             
        #if there is only one possible split between the current section (cell length = 4)
-       if currNode.coordinates[0][1]-currNode.coordinates[0][0] == 4:
+        if currNode.coordinates[0][1]-currNode.coordinates[0][0] == 4:
+            print("final split")
             #set it so that the split is equivalent of to have the cell coordinate be size 1
             currNode.split1=Node(currNode.coordinates[0][0], currNode.coordinates[0][0]+2, currNode.coordinates[1][0], currNode.coordinates[1][1])
             print(currNode.split1.coordinates)
             currNode.split2=Node(currNode.coordinates[0][0]+2, currNode.coordinates[0][1], currNode.coordinates[1][0], currNode.coordinates[1][1])
             print(currNode.split2.coordinates)
-            
-        else:
 
-            randSplit = random.randrange(currNode.coordinates[0][0], currNode[0][1],2)
+        #if the split selected causes it to split on itself
+        elif currNode.coordinates[0][1]-currNode.coordinates[0][0]==2:
+             #if current section is already part of the minimum size of cell 
+            #then stop the split and return to previous parent node to continue the split
+            print("Returning to previous node due to no more possible splits")
+        
+        else:
+            #check to make sure that origin line coordinate ranges are not selected for the split (ex: [0][0])
+            tmpLower = 2
+            if(currNode.coordinates[0][0]!=0):
+                tmpLower = currNode.coordinates[0][0]
+
+
+            randSplit = random.randrange(tmpLower, currNode.coordinates[0][1],2)
         
             #adjust split for situation where a number is picked such that it is equal to the last edge of the rows
             #if randSplit+1 == currNode.coordinates[0][1] or randSplit == currNode.coordinates[0][0]:
@@ -80,7 +93,7 @@ def binarySP(rows, columns, currNode):
             
 
             print("rand "+str(randSplit) + "\n") #outputs the coordinate that will be split among them
-                #set the two nodes child nodes
+            #set the two nodes child nodes
             
             
             currNode.split1 = Node(currNode.coordinates[0][0],randSplit,currNode.coordinates[1][0] ,currNode.coordinates[1][1])
@@ -94,20 +107,33 @@ def binarySP(rows, columns, currNode):
             print ("split2RandH")
             binarySP(currNode.split2.coordinates[0][1], currNode.split2.coordinates[1][1], currNode.split2)
 
-        #if split happens vertically    
+    #if split happens vertically    
     elif selectRandSplit == 2:
 
-            #if there is only one possible split between the current section (cell length = 2)
+        #if there is only one possible split between the current section (cell length = 2)
         if currNode.coordinates[1][1]-currNode.coordinates[1][0] == 4:
+            print("final split")
             #set it so that the split is equivalent of to have the cell coordinate be size 1
             currNode.split1=Node(currNode.coordinates[0][0], currNode.coordinates[0][1], currNode.coordinates[1][0], currNode.coordinates[1][0]+2)
             print(currNode.split1.coordinates)
             currNode.split2=Node(currNode.coordinates[0][0], currNode.coordinates[0][1], currNode.coordinates[1][0]+2, currNode.coordinates[1][1])
             print(currNode.split2.coordinates)
+        
+        elif currNode.coordinates[1][1]-currNode.coordinates[1][0] == 2:
+             #if current section is already part of the minimum size of cell 
+            #then stop the split and return to previous parent node to continue the split
+            print("Returning to previous node due to no more possible splits")
+
             
         else:
-                #get the random coordinate between the range of the current node to split between for the children
-            randSplit = random.randrange(currNode.coordinates[1][0], currNode[1]][1],2)
+
+            #check to make sure that origin line coordinate ranges are not selected for the split (ex: [0][0])
+            tmpLower = 2
+            if(currNode.coordinates[1][0]!=0):
+                tmpLower = currNode.coordinates[1][0]
+            
+            #get the random coordinate between the range of the current node to split between for the children
+            randSplit = random.randrange(tmpLower, currNode.coordinates[1][1],2)
 
             print("rand "+str(randSplit) + "\n")
 
@@ -121,10 +147,10 @@ def binarySP(rows, columns, currNode):
             print(currNode.split2.coordinates)
 
             #start the recursive calls for generating the next child nodes
-        print ("split1RandV")
-        binarySP(currNode.split1.coordinates[0][1], currNode.split1.coordinates[1][1], currNode.split1)
-        print ("split2RandV")
-        binarySP(currNode.split2.coordinates[0][1], currNode.split2.coordinates[1][1], currNode.split2)
+            print ("split1RandV")
+            binarySP(currNode.split1.coordinates[0][1], currNode.split1.coordinates[1][1], currNode.split1)
+            print ("split2RandV")
+            binarySP(currNode.split2.coordinates[0][1], currNode.split2.coordinates[1][1], currNode.split2)
 
 
 
