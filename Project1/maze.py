b0a8d199e37848e8975c0d444285acaca5b2c48a
Brian <ballerbrian4@gmail.com>
2018-02-14 23:40:04 -0800

    added functions to generate portals

--- Project1/maze.py
+++ Project1/maze.py
@@ -15,12 +15,6 @@ columnsI = int(columnsA)*2
 #variable to hold coordinates for portals when generating maze
 mazePortals = []
 
-#variables 
-#rowSize = (int(rowsA) * 2) + 1
-#columnSize=(int(columnsA) * 2) +1
-#print(rowSize)
-#print(columnSize)
-
 # global variables to print maze character 
 #cornerChar = "+"
 #horizontalChar = "-"
@@ -41,11 +35,10 @@ class Node:
 
 
 #recursive function to construct the BSP tree
-def binarySP(rows, columns, currNode):
+def binarySP(rows, columns, currNode, pList):
     
     print("currCoordinates:")
     print(currNode.coordinates)
-    
 
     
     selectRandSplit = random.randint(1,2)
@@ -54,109 +47,17 @@ def binarySP(rows, columns, currNode):
     #if split happens horizontally
     if selectRandSplit == 1:
        #get the random coordinate between the range of the current node to split betweeen for the children            
-       horizontalSplit(currNode)     
-       ''' #if there is only one possible split between the current section (cell length = 4)
-        if currNode.coordinates[0][1]-currNode.coordinates[0][0] == 4:
-            print("final split for this direction-section")
-            #set it so that the split is equivalent of to have the cell coordinate be size 2
-            currNode.split1=Node(currNode.coordinates[0][0], currNode.coordinates[0][0]+2, currNode.coordinates[1][0], currNode.coordinates[1][1])
-            print(currNode.split1.coordinates)
-            currNode.split2=Node(currNode.coordinates[0][0]+2, currNode.coordinates[0][1], currNode.coordinates[1][0], currNode.coordinates[1][1])
-            print(currNode.split2.coordinates)
-
-        #if the split selected causes it to split on itself
-        elif currNode.coordinates[0][1]-currNode.coordinates[0][0]==2:
-            #check if the current node still has range to split for opposite direction
-            if currNode.coordinates[1][1] - currNode[1][0] != 2:
-                #call split function to split opposite direction for region
-
-
-            
-            
-        #covers final case where cell can no longer be split upon itself
-        elif currNode.coordinates[0][1]-currNode.coordinates[0][0]==2 and currNode.coordinates[1][1]-currNode.coordinates[1][0]==2:
-            #then stop the split and return to previous parent node to continue the split function
-            print("Returning to previous node due to no more possible splits")
-
-        
-        else:
-            #check to make sure that origin line coordinate ranges are not selected for the split (ex: [0][0])
-            tmpLower = 2
-            if(currNode.coordinates[0][0]!=0):
-                tmpLower = currNode.coordinates[0][0]
-
-
-            randSplit = random.randrange(tmpLower, currNode.coordinates[0][1],2)
-        
-            #adjust split for situation where a number is picked such that it is equal to the last edge of the rows
-            #if randSplit+1 == currNode.coordinates[0][1] or randSplit == currNode.coordinates[0][0]:
-            #    randSplit = random.randint(currNode.coordinates[0][0]+1, rows-1) #untested condition
-
-            
-
-            print("rand "+str(randSplit) + "\n") #outputs the coordinate that will be split among them
-            #set the two nodes child nodes
-            
-            
-            currNode.split1 = Node(currNode.coordinates[0][0],randSplit,currNode.coordinates[1][0] ,currNode.coordinates[1][1])
-            print(currNode.split1.coordinates)
-            currNode.split2 = Node(randSplit, currNode.coordinates[0][1],currNode.coordinates[1][0] ,currNode.coordinates[1][1])
-            print(currNode.split2.coordinates)
-
-            #start the recursive calls for the child nodes to develop the granchildren
-            print ("split1RandH")
-            binarySP(currNode.split1.coordinates[0][1], currNode.split1.coordinates[1][1], currNode.split1)
-            print ("split2RandH")
-            binarySP(currNode.split2.coordinates[0][1], currNode.split2.coordinates[1][1], currNode.split2) '''
+       horizontalSplit(currNode,pList)     
+       
 
     #if split happens vertically    
     elif selectRandSplit == 2:
-        verticalSplit(currNode)
-        '''  #if there is only one possible split between the current section (cell length = 2)
-        if currNode.coordinates[1][1]-currNode.coordinates[1][0] == 4:
-            print("final split")
-            #set it so that the split is equivalent of to have the cell coordinate be size 1
-            currNode.split1=Node(currNode.coordinates[0][0], currNode.coordinates[0][1], currNode.coordinates[1][0], currNode.coordinates[1][0]+2)
-            print(currNode.split1.coordinates)
-            currNode.split2=Node(currNode.coordinates[0][0], currNode.coordinates[0][1], currNode.coordinates[1][0]+2, currNode.coordinates[1][1])
-            print(currNode.split2.coordinates)
-        
-        elif currNode.coordinates[1][1]-currNode.coordinates[1][0] == 2:
-             #if current section is already part of the minimum size of cell 
-            #then stop the split and return to previous parent node to continue the split
-            print("Returning to previous node due to no more possible splits")
-
-            
-        else:
-
-            #check to make sure that origin line coordinate ranges are not selected for the split (ex: [0][0])
-            tmpLower = 2
-            if(currNode.coordinates[1][0]!=0):
-                tmpLower = currNode.coordinates[1][0]
-            
-            #get the random coordinate between the range of the current node to split between for the children
-            randSplit = random.randrange(tmpLower, currNode.coordinates[1][1],2)
-
-            print("rand "+str(randSplit) + "\n")
-
-            
-                #currNode.split1 = Node(currNode.coordinates[0][0],randSplit,currNode.coordinates[1][0] ,currNode.coordinates[1][1])
-                #currNode.split2 = Node(randSplit+1, currNode.coordinates[0][1],currNode.coordinates[1][0] ,currNode.coordinates[1][1])
-            currNode.split1 = Node(currNode.coordinates[0][0], currNode.coordinates[0][1],currNode.coordinates[1][0] ,randSplit)
-            print(currNode.split1.coordinates)
-
-            currNode.split2 = Node(currNode.coordinates[0][0], currNode.coordinates[0][1],randSplit,currNode.coordinates[1][1])
-            print(currNode.split2.coordinates)
-
-            #start the recursive calls for generating the next child nodes
-            print ("split1RandV")
-            binarySP(currNode.split1.coordinates[0][1], currNode.split1.coordinates[1][1], currNode.split1)
-            print ("split2RandV")
-            binarySP(currNode.split2.coordinates[0][1], currNode.split2.coordinates[1][1], currNode.split2) '''
+        verticalSplit(currNode,pList)
+       
 
 
 #function to define horizontal split
-def horizontalSplit(nodeToSplit):
+def horizontalSplit(nodeToSplit, portalList):
     #get the random coordinate between the range of the current node to split betweeen for the children            
             
     #if there is only one possible split between the current section (cell length = 4)
@@ -168,11 +69,19 @@ def horizontalSplit(nodeToSplit):
         nodeToSplit.split2=Node(nodeToSplit.coordinates[0][0]+2, nodeToSplit.coordinates[0][1], nodeToSplit.coordinates[1][0], nodeToSplit.coordinates[1][1])
         print(nodeToSplit.split2.coordinates)
 
+        #find the cross section of wall to remove to act as the portal
+        port = random.randrange(nodeToSplit.coordinates[1][0]+1, nodeToSplit.coordinates[1][1],2)
+
+        portalCoord = [port,nodeToSplit.coordinates[0][0]+2]
+
+        portalList.append(portalCoord)
+
+
          #start the recursive calls for the child nodes to develop the granchildren
         print ("split1RandH")
-        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1)
+        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1, portalList)
         print ("split2RandH")
-        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2)
+        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2, portalList)
 
 
 
@@ -181,7 +90,7 @@ def horizontalSplit(nodeToSplit):
         #check if the current node still has range to split for opposite direction
         if nodeToSplit.coordinates[1][1] - nodeToSplit.coordinates[1][0] != 2:
             #call split function to split opposite direction for region
-            verticalSplit(nodeToSplit)
+            verticalSplit(nodeToSplit, portalList)
 
 
             
@@ -203,9 +112,13 @@ def horizontalSplit(nodeToSplit):
         while randSplit == tmpLower:
             randSplit = random.randrange(tmpLower, nodeToSplit.coordinates[0][1],2)
         
-        #adjust split for situation where a number is picked such that it is equal to the last edge of the rows
-        #if randSplit+1 == currNode.coordinates[0][1] or randSplit == currNode.coordinates[0][0]:
-        #    randSplit = random.randint(currNode.coordinates[0][0]+1, rows-1) #untested condition
+
+        #picking the portal to open in between the two regions and push to the portals list
+        port = random.randrange(nodeToSplit.coordinates[1][0]+1, nodeToSplit.coordinates[1][1],2)
+
+        #assign it and then append it to the portalList
+        portalCoord = [port, randSplit]
+        portalList.append(portalCoord)
 
             
 
@@ -220,13 +133,13 @@ def horizontalSplit(nodeToSplit):
 
         #start the recursive calls for the child nodes to develop the granchildren
         print ("split1RandH")
-        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1)
+        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1, portalList)
         print ("split2RandH")
-        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2)
+        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2, portalList)
 
 
 #function to define vertical split
-def verticalSplit(nodeToSplit):
+def verticalSplit(nodeToSplit, portalList):
     #if there is only one possible split between the current section (cell length = 2)
     if nodeToSplit.coordinates[1][1]-nodeToSplit.coordinates[1][0] == 4:
         print("final split")
@@ -236,18 +149,27 @@ def verticalSplit(nodeToSplit):
         nodeToSplit.split2=Node(nodeToSplit.coordinates[0][0], nodeToSplit.coordinates[0][1], nodeToSplit.coordinates[1][0]+2, nodeToSplit.coordinates[1][1])
         print(nodeToSplit.split2.coordinates)
 
+        #find the cross section of wall to remove to act as the portal
+        port = random.randrange(nodeToSplit.coordinates[0][0]+1, nodeToSplit.coordinates[0][1],2)
+
+        portalCoord = [nodeToSplit.coordinates[1][0]+2,port]
+
+        portalList.append(portalCoord)
+
+
+
         #start the recursive calls for generating the next child nodes
         print ("split1RandV")
-        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1)
+        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1,portalList)
         print ("split2RandV")
-        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2)
+        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2, portalList)
 
     #if the split selected causes it to split on itself
     elif nodeToSplit.coordinates[1][1]-nodeToSplit.coordinates[1][0]==2:
         #check if the current node still has range to split for opposite direction
         if nodeToSplit.coordinates[0][1] - nodeToSplit.coordinates[0][0] != 2:
             #call split function to split opposite direction for region
-            horizontalSplit(nodeToSplit)
+            horizontalSplit(nodeToSplit, portalList)
 
 
 
@@ -256,7 +178,6 @@ def verticalSplit(nodeToSplit):
          #if current section is already part of the minimum size of cell 
         #then stop the split and return to previous parent node to continue the split
         print("Returning to previous node due to no more possible splits")
-
             
     else:
 
@@ -272,6 +193,15 @@ def verticalSplit(nodeToSplit):
         while randSplit == tmpLower:
             randSplit = random.randrange(tmpLower, nodeToSplit.coordinates[1][1],2)
         
+
+        #portal selection in the split
+        port = random.randrange(nodeToSplit.coordinates[0][0]+1, nodeToSplit.coordinates[0][1],2)
+
+        #assign it and then append it to the portalList
+        portalCoord = [randSplit,port]
+        portalList.append(portalCoord)
+
+
         print("rand "+str(randSplit) + "\n")
 
             
@@ -285,9 +215,9 @@ def verticalSplit(nodeToSplit):
 
         #start the recursive calls for generating the next child nodes
         print ("split1RandV")
-        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1)
+        binarySP(nodeToSplit.split1.coordinates[0][1], nodeToSplit.split1.coordinates[1][1], nodeToSplit.split1, portalList)
         print ("split2RandV")
-        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2)
+        binarySP(nodeToSplit.split2.coordinates[0][1], nodeToSplit.split2.coordinates[1][1], nodeToSplit.split2,portalList)
 
 
 
@@ -307,10 +237,13 @@ def printTreeLeaves(cNode):
 
 treeRoot = Node(0, rowsI, 0, columnsI)
 
-binarySP(rowsI, columnsI, treeRoot)
+binarySP(rowsI, columnsI, treeRoot, mazePortals)
+
+for z in mazePortals:
+    print(z)
 
 
-print("Printing leaves of bspTree:")
-printTreeLeaves(treeRoot)
+# print("Printing leaves of bspTree:")
+# printTreeLeaves(treeRoot)
 
 #beginSpace = Node(int(rowsA), int(columnsA)
